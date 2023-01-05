3e82cb3d394f884991b1c1efdc9ea250d88309cd
gerhidalgo <germhidalgo@gmail.com>
2023-01-04 23:03:06 -0300

    fin 1ra entrega

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//Preguntemos precio
+//Variables
 let price = 0
 let divideBetween = 0
 let dividedPrice = 0
 let names = []
 let input = 0
 let i = 0
+
+//Preguntemos precio
 function askPrice(){
     price = prompt("Ingrese el monto a pagar")
     while (isNaN(price)) {
@@ -13,43 +15,45 @@ function askPrice(){
     }
     tips()
 }
-//Propina?
+
+//¿Propina?
 function tips(){
     let decision = prompt("Desea dejar propina? S/N")
     if (decision.toLowerCase()=='s'){
-        let percentage = prompt("Ingrese el porcentaje (10=10%)")
+        let percentage = parseInt(prompt("Ingrese el porcentaje (10=10%)")) 
         while (isNaN(percentage)) {
             alert("El valor ingresado no es un número, reintente")
             tips()
         }
         let priceWTips = (price*(percentage/100))
-        price = price + priceWTips
+        price = (+price + +priceWTips)
+        toDivide()
+    } else if (decision.toLowerCase()=='n'){
         toDivide()
     } else {
-        toDivide()
+        alert("El caracter ingresado no es una opción válida")
+        tips()
     }
      
 }
-//Cuantos son
+
+//¿Cuantos son?
 function toDivide() {
     let decision = prompt("Paga solo? S/N")
     if (decision.toLowerCase()=='s'){
-        showPrice()
-    } else {
-        divideBetween = prompt("¿Cuántos son?")
-        console.log(divideBetween)
-        while (isNaN(divideBetween)) {
-            alert("El valor ingresado no es un número, reintente")
-            
-        }
-        divide()
-        showPrice()
+        showPriceIndividual()
+    } else if (decision.toLowerCase()=='n'){
         askNames()
+        divide()
+        showPriceGroup()
+    } else {
+        alert("El caracter ingresado no es una opción válida")
+        toDivide()
     }
 }
-//Nombre de los participantes
+//¿Nombre de los participantes?
 function askNames() {
-    input = prompt("Ingrese el nombre del participante")
+    input = prompt(`Ingrese el nombre del participante n°${i + 1} (colocar '0' para no agregar mas)`)
     addNames()
 }
 function addNames(){
@@ -58,23 +62,38 @@ function addNames(){
         names.push(input)
         askNames()
     }
-    console.log(names)
+    //console.log(names) check funcionamiento
 }
+
 //Dividir
 function divide(){
-    dividedPrice = price/divideBetween
+    dividedPrice = price/(names.length)
 }
-//Cuanto puso cada uno, hacer con nombres 
-//Quien debe a quien
+//Cuanto puso cada uno, hacer con nombres (proximamente)
+//Quien debe a quien (proximamente)
+//Realizar grafico (proximamente)
+//Almacenar varias cuentas (proximamente)
+//Tipo de cuentas (Comida, Transporte, Alquiler, Otro, etc)(proximamente)
 
 //Fin
-function showPrice() {
-    if (divideBetween=0) {
-        console.log(`El precio individual es ${price}`)
+function showPriceIndividual() {
+    alert(`El precio a pagar de manera individual es ${price}`)
+    let decision = prompt("¿Desea realizar otra cuenta? S/N")
+    if (decision.toLowerCase()=='s'){
+        askPrice()
     } else {
-        console.log(`El precio a pagar entre ${divideBetween} personas es ${dividedPrice} c/u`)
-        //No toma el valor de la variable
+        console.log("Fin")
     }
 }
+function showPriceGroup() {
+    alert(`El precio es: ${price}, a pagar entre ${(names.length)} es: ${dividedPrice} c/u`)
+    let decision = prompt("¿Desea realizar otra cuenta? S/N")
+    if (decision.toLowerCase()=='s'){
+        askPrice()
+    } else {
+        console.log("Fin")
+    }
+}
+
 //Iniciamos
 askPrice()
