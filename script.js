46582439aa9e205872e76beb1764b7943dac6d1f
German Hidalgo <ghidalgo@clinicadecuyo.com.ar>
2023-01-18 13:20:27 -0300

    funcional

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let deudaResult = []
 let participants = []
 let cobrarResult = []
 let input = 0
-let times = 0
 let timesEdit = 0
 
 
@@ -54,46 +53,45 @@ class Participante {
     }
 }
 
-function agregarParticipante() {
-    if (times <= 1) {
-        input = prompt(`Desea agregar un participante? S/N`)
-        if (input.toLowerCase() == 's') {
-            let nombreInput = prompt(`Ingrese el nombre del participante n${participants.length + 1}`)
-            let montoInput = prompt(`¿Cuanta plata puso ${nombreInput}?`)
-            const participanteNuevo = new Participante(participants.length + 1, nombreInput, montoInput)
-            console.log(participanteNuevo)
-            participants.push(participanteNuevo)
-            agregarParticipante()
-            //participanteNuevo.mostrarInfoEstado()
-            //console.log(participants)
-        } else if (input.toLowerCase() == 'n') {
-            divide()
-        } else {
-            alert(`${input} no es una opción válida`)
-            agregarParticipante()
-        }
-    } else {
+function agregarParticipante(optionStart) {
+    let times =+ optionStart
+    console.log(times)
+    if (times >= 1){
         participants = []
-        times = 0
+    }
+    input = prompt(`Desea agregar un participante? S/N`)
+    if (input.toLowerCase() == 's') {
+        let nombreInput = prompt(`Ingrese el nombre del participante n${participants.length + 1}`)
+        let montoInput = prompt(`¿Cuanta plata puso ${nombreInput}?`)
+        const participanteNuevo = new Participante(participants.length + 1, nombreInput, montoInput)
+        console.log(participanteNuevo)
+        participants.push(participanteNuevo)
+        agregarParticipante()
+        //participanteNuevo.mostrarInfoEstado()
+        //console.log(participants)
+    } else if (input.toLowerCase() == 'n') {
+        divide()
+    } else {
+        alert(`${input} no es una opción válida`)
         agregarParticipante()
     }
-
 }
 
-//Dividir
-function divide() {
-    dividedPrice = price / (participants.length)
-}
 
-//Inicio
-function menu() {
-    let exitMenu = false
-    do {
-        exitMenu = preguntarOpcion(exitMenu)
-    } while (!exitMenu)
-}
-function preguntarOpcion(salir) {
-    let optionStart = parseInt(prompt(`Ingrese la opcion deseada
+    //Dividir
+    function divide() {
+        dividedPrice = price / (participants.length)
+    }
+
+    //Inicio
+    function menu() {
+        let exitMenu = false
+        do {
+            exitMenu = preguntarOpcion(exitMenu)
+        } while (!exitMenu)
+    }
+    function preguntarOpcion(salir) {
+        let optionStart = parseInt(prompt(`Ingrese la opcion deseada
         1. Iniciar nueva división
         2. Ver resultado ultima división
         3. Modificar divisiones previas
@@ -101,50 +99,50 @@ function preguntarOpcion(salir) {
         5. Ver todas las divisiones (no)
         0. Salir`))
 
-    switch (optionStart) {
-        case 1:
-            times++
-            askPrice()
-            agregarParticipante()
-            deber()
-            showPrice()
-            break
-        case 2:
-            showPrice()
-            break
-        case 3:
-            edit(participants, optionStart)
-            deber()
-            showPrice()
-            timesEdit++
-            break
-        case 4:
-            edit(participants, optionStart)
-            deber()
-            showPrice()
-            break
-        case 5:
-            console.log(`Todas`)
-            break
-        case 0:
-            console.log(`Adios`)
-            salir = true
-            return salir
-            break
-        default:
-            alert(`${optionStart} no es una opción válida`)
-            break
+        switch (optionStart) {
+            case 1:
+                askPrice()
+                agregarParticipante(optionStart)
+                deber()
+                showPrice()
+                break
+            case 2:
+                showPrice()
+                break
+            case 3:
+                timesEdit++
+                edit(participants, optionStart)
+                deber()
+                showPrice()
+                break
+            case 4:
+                edit(participants, optionStart)
+                deber()
+                showPrice()
+                break
+            case 5:
+                console.log(`Todas`)
+                break
+            case 0:
+                console.log(`Adios`)
+                salir = true
+                return salir
+                break
+            default:
+                alert(`${optionStart} no es una opción válida`)
+                break
+        }
     }
-}
-//Quien debe a quien
-function deber() {
-    console.log(times)
-    if (times = 0) {
+    //Quien debe a quien
+    function deber() {
+        if ((timesEdit = 1)) {
+            cobrarResult = []
+            deudaResult = []
+        }
         for (const participanteNuevo of participants) {
             participanteNuevo.debe = dividedPrice - participanteNuevo.monto
             //NO DEBE NADA
-            if (participanteNuevo.debe <= 0) {
-                participanteNuevo.debe = 0
+            if (participanteNuevo.debe < 0) {
                 cobrarResult.push(`${participanteNuevo.nombre}`)
             } else {
                 deudaResult.push(` ${participanteNuevo.nombre} debe ${participanteNuevo.debe}`)
@@ -152,68 +150,64 @@ function deber() {
             console.log(participanteNuevo.id)
             console.log(participanteNuevo.debe)
         }
-    } else {
-        cobrarResult = []
-        deudaResult = []
-        times = 0
-        deber()
     }
+    function cobrar() {
 
-}
-
-function cobrar() {
-
-    alert(``)
-}
-//Modificar Nombre o Monto
-function edit(array, optionStart) {
-    if (participants == '') {
-        alert(`No hay ediciones que realizar aun`)
-        return
-    } else {
-        let updParticipantM = 0
-        let updParticipantN = ''
-        if (optionStart == 3) {
-            updParticipantM = 0
-            updParticipantN = ''
+        alert(``)
+    }
+    //Modificar Nombre o Monto
+    function edit(array, optionStart) {
+        if (timesEdit = 1) {
             deudaResult = []
-            let searchedName = prompt(`Ingrese nombre del participante a cambiar monto`)
-            let foundedID = array.findIndex((participante) => participante.nombre.toLowerCase() == searchedName.toLowerCase())
-            updParticipantM = array[foundedID].monto = prompt('Reingrese monto')
-        } else {
-            deudaResult = []
-            updParticipantM = 0
-            updParticipantN = ''
-            let searchedName = prompt(`Ingrese el nombre que desea cambiar`)
-            let foundedID = array.findIndex((participante) => participante.nombre.toLowerCase() == searchedName.toLowerCase())
-            updParticipantN = array[foundedID].nombre = prompt('Reingrese nombre')
+            cobrarResult = []
         }
+        if (participants == '') {
+            alert(`No hay ediciones que realizar aun`)
+            return
+        } else {
+            let updParticipantM = 0
+            let updParticipantN = ''
+            if (optionStart == 3) {
+                updParticipantM = 0
+                updParticipantN = ''
+                let searchedName = prompt(`Ingrese nombre del participante a cambiar monto`)
+                let foundedID = array.findIndex((participante) => participante.nombre.toLowerCase() == searchedName.toLowerCase())
+                updParticipantM = array[foundedID].monto = prompt('Reingrese monto')
+                timesEdit = 0
+            } else {
+                updParticipantM = 0
+                updParticipantN = ''
+                let searchedName = prompt(`Ingrese el nombre que desea cambiar`)
+                let foundedID = array.findIndex((participante) => participante.nombre.toLowerCase() == searchedName.toLowerCase())
+                updParticipantN = array[foundedID].nombre = prompt('Reingrese nombre')
+                timesEdit = 0
+            }
+        }
+
+
+        console.log(participants)
     }
+    //Realizar grafico (proximamente)
+    //Almacenar varias cuentas (proximamente)
+    //Tipo de cuentas (Comida, Transporte, Alquiler, Otro, etc)(proximamente)
 
+    //Fin
 
-    console.log(participants)
-}
-//Realizar grafico (proximamente)
-//Almacenar varias cuentas (proximamente)
-//Tipo de cuentas (Comida, Transporte, Alquiler, Otro, etc)(proximamente)
-
-//Fin
-
-function showPrice() {
-    if (price == 0) {
-        alert(`No hay divisiones previas`)
-        return
-    }
-    if (participants.length == 1) {
-        alert(`El precio es: ${price}, a pagar individualmente es: ${dividedPrice}`)
-    } else {
-        alert(`El precio es: ${price}, a pagar entre ${(participants.length)} es: ${dividedPrice} c/u`)
-        if (deudaResult == '') {
+    function showPrice() {
+        if (price == 0) {
+            alert(`No hay divisiones previas`)
             return
         }
-        alert(`${deudaResult} a ${cobrarResult}`)
+        if (participants.length == 1) {
+            alert(`El precio es: ${price}, a pagar individualmente es: ${dividedPrice}`)
+        } else {
+            alert(`El precio es: ${price}, a pagar entre ${(participants.length)} es: ${dividedPrice} c/u`)
+            if (deudaResult == '') {
+                return
+            }
+            alert(`${deudaResult} a ${cobrarResult}`)
+        }
     }
-}
-//Iniciamos
-menu()
+    //Iniciamos
+    menu()
 
